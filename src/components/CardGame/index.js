6ad6acd06700ac89ba88d5ba299f9c02984d96f9
Sharkyer1 <nathantrabson@gmail.com>
2021-11-30 16:19:35 -0300

    :rocket: First commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+  return `
+    <article class="card-game">
+      <img src="images/creper-pixel.png" alt="Creper em pixel">     
+    </article>
+  `;
+}
+
+export default CardGame;
