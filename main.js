8aae8dfeb71b826539ec46bfd395f0a3b7205bf3
Sharkyer1 <nathantrabson@gmail.com>
2021-12-16 13:25:03 -0300

    :construction: Create the VsPlayer component without CSS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import "./src/styles/Elements/base.css";
 import ScoreBoard from "./src/Objects/ScoreBoard";
 import PlayerName from "./src/components/PlayerName";
 import BoardGame from "./src/Objects/BoardGame";
+import VsPlayer from "./src/components/VsPlayer";
 
 
 const $root = document.querySelector("#root");
@@ -11,7 +12,7 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML(
     "beforeend", 
     `
-       ${ScoreBoard()}  
+       ${ScoreBoard()} 
        ${BoardGame(6)}
     `
     
